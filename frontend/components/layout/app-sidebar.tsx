1e430cd8a50d73a05b7b9e8331706816cb81cfc0
Adhirararaj <adhirajghadge11@gmail.com>
2025-06-04 20:57:19 +0530

    ui changes

--- frontend/components/layout/app-sidebar.tsx
+++ frontend/components/layout/app-sidebar.tsx
@@ -42,7 +42,7 @@ export function AppSidebar() {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <a href="/manager">
+              <a href="/student">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-primary text-primary-foreground">
                   <Home className="size-4" />
                 </div>
